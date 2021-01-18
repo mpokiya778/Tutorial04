c31392c1fa88c8f10ad81322a0e9598b9d3d9363
DELL <mpokiya778@rku.ac.in>
2021-01-18 21:52:22 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/tutorial04/Splash.java
@@ -0,0 +1,29 @@
+package com.example.tutorial04;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        TimerTask timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Splash.this,MainActivity.class);
+                startActivity(intent);
+            }
+        };
+
+        Timer timer = new Timer();
+        timer.schedule(timerTask,2000);
+    }
+}
